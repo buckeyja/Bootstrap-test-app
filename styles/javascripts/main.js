b42b7c750efd4c2370956fac5f970fa92da85227
James Buckey <jamesabuckey@gmail.com>
2019-08-28 15:37:14 -0400

    feat: add menu button for mobile screens

---
+++ styles/javascripts/main.js
@@ -0,0 +1,11 @@
+/*var navButton = document.getElementById('switch-button');
+
+// Display drop-down menu
+navButton.addEventListener('click', displayDropdown);
+
+// Show drop-down
+function displayDropdown(e){
+  // Change menu icon
+  var buttonImg = document.getElementById('btnImg');
+  buttonImg.setAttribute('src', './images/close.svg');
+}*/
